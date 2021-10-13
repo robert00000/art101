4c41aa7869d33f3d7d8a8b8a384c12883063d0ce
Robert Williams <boewilliam@gmail.com>
2021-10-12 19:15:57 -0700

    Added a css stylesheet to lab 4 as well as comments in the js file. Index.html for lab 4 has been given paragraphs and headers for the assignment and an image has been added to the document.

---
+++ lab4/js/lab.js
@@ -0,0 +1,26 @@
+// Summary. Basically what is here is not real code but pseudocode.
+// This is Pseudocoding for Lab 4.
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author Robert Williams.
+// @since  10.12.21
+
+//Task making a html project
+  //Outline for project
+  //Create project file
+  //Create html file and folders for css and the other respective items.
+  //Write or copy and outline for a website in html file
+//2) Writing code into html file
+  //Create a header and write the title for the project for the file
+  //Add in any images that need to be inside of the project through linking them
+  //Make sure to commit and push in case you need to revert any changes.
+  //Create paragraphs describing the different images on the page or what the page is about
+//3) Write code in css file.
+  //Add a stylesheet
+  //Change fonts of different text and the size
+  //Adjust size of images and boxes
+//4) Link files together, push it to github.
+
+/*
+Voila! :o
+*/
