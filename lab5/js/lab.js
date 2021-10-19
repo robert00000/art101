d7ae12bece922e649ad385ddb23c24119ec9a397
Robert Williams <boewilliam@gmail.com>
2021-10-18 22:28:18 -0700

    Lab 5 submission
    Changed the folders html file to show lab 5. Did the lab 5 assignment.

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/*
+* Author: Robert Williams <roewilli@ucsc.edu
+* Created 10/18/21
+* License: Public Domain
+*/
+
+//Define Variables
+make = "Ford";
+model = "Taurus";
+color = "Rusty";
+year = 1995;
+age = 2019 - year;
+
+// calculate
+age = 2019 - year;
+
+//output
+document.writeln("Make: " + make + "<br");
+document.writeln("Model: " + model + "<br");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>")
