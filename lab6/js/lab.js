dceb841fc98c29398ddac08320adb6960b9e1d42
Robert Williams <boewilliam@gmail.com>
2021-10-20 23:48:37 -0700

    Finished lab 6

---
+++ lab6/js/lab.js
@@ -0,0 +1,22 @@
+/**
+ * Author:    Robert Williams
+ * Created:   10.20.2021
+ *
+ * (c) Copyright by Blub Corp.
+ **/
+
+let myTransport = ['Car', 'Bus', 'Bike', 'Train'];
+let myMainRide = ['Ford', 'Taurus', 'Rusty', 1995, 26];
+myTransport[0];
+myTransport[1];
+myTransport[2];
+myTransport[3];
+myTransport[4];
+
+myMainRide[0];
+myMainRide[1];
+myMainRide[2];
+myMainRide[3];
+
+document.writeln("Getting around: " + myTransport + "<br>");
+document.writeln("My main ride:" + myMainRide + "<br>");
