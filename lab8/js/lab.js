94787d65ffff4b3623b39c23f6c38d8fdbaf39b0
Robert Williams <boewilliam@gmail.com>
2021-10-27 22:58:14 -0700

    Linked lab8 to html

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -12,8 +12,10 @@
  var array = [3,5,8,20,4,10];
  //These show the output in the consoles.
  console.log("The output of the function when 2 is inputted.", powerOf2(2));
+ //Another example to show that the function works.
  console.log("The output of the function when 2 is inputted.", powerOf2(11));
+ //Makes sure to show the result of the function in an array.
  console.log("The result from running array.map", array.map(powerOf2));
- var outputEl = document.getElementById("output");
- var output = ("This is the original array: " + array +"This is my output: " + array.map(powerOf2));
- outputEl.innerHTML = output; // put your results here
+ var outputEl = document.getElementById("output"); //This allows the html document to access the output and place it into the div.
+ var output = ("This is the original array: " + array +"This is my output: " + array.map(powerOf2)); //This variable puts a string for the output.
+ outputEl.innerHTML = output; // puts your results here
