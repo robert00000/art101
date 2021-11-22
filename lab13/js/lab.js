d0c157596453d726a0b5bb59ee1a63b1892bb241
Robert Williams <roewilli@ucsc.edu>
2021-11-21 21:41:36 -0800

    Finished lab 13.
    Might touch up on the looks of the page but it's otherwise finished.

---
+++ lab13/js/lab.js
@@ -0,0 +1,55 @@
+/**
+ * Author:    Robert Williams
+ * Created:   11.20.2021
+ *
+ *
+ **/
+outputEl = document.getElementById("output");
+$("#challenge").toggleClass("padding")
+$("#problem").toggleClass("padding")
+$("#results").toggleClass("padding")
+$("#output").toggleClass("padding")
+//This outputs the code to the page
+function outputToPage(str) {
+    newEl = document.createElement("p");
+    newEl.innerHTML = str;
+    outputEl.appendChild(newEl);
+}
+function mod(){
+
+  for(i=1;i <= 200;i++){
+    var outputStr = "";
+    if(i % 3 == 0 && i%5 == 0){
+      console.log("fizzbuzz");
+
+    }
+    else if(i%15 == 0){
+      console.log("fizzbuzzboom");
+      outputToPage("fizzbuzzboom" + outputStr);
+    }
+    else if(i%7 == 0){
+      console.log("boom");
+      outputToPage("boom" + outputStr);
+    }
+    else if(i%5 == 0){
+      console.log("buzz");
+      outputToPage("buzz" + outputStr);
+    }
+    else if(i%3 == 0){
+      console.log("fizz");
+      outputToPage("fizz" + outputStr);
+    }
+    else{
+      console.log(i);
+      outputToPage(i.toString() + outputStr);
+    }
+
+  }
+  //$("#output").text("Number " + mod());
+}
+$("#button").click(function(){
+  console.log("Button pushed")
+  outputEl.innerHTML = "";
+  $("#output").text(mod());
+
+})
