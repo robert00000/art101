2173304a4ca834145944c7d734c288f8af07ea8d
Robert Williams <boewilliam@gmail.com>
2021-11-29 22:35:24 -0800

    Finished lab 15

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -4,3 +4,38 @@
  *
  *
  **/
+ // Using the core $.ajax() method
+ //var myUrl = "http://numbersapi.com/42/math";
+//var myUrl = "https://api.kanye.rest/";
+//var myUrl = "https://cataas.com/cat?json=true";
+ var myUrl = "http://numbersapi.com/11/29/date";
+ $("#content").toggleClass("padding");
+ $("#activate").click(function(){
+   $.ajax({
+       // The URL for the request (from the api docs)
+       url: myUrl,
+       // The data to send (will be converted to a query string)
+       // data: {
+       //         // here is where any data required by the api
+       //         //   goes (check the api docs)
+       //         id: 123,
+       //         api_key: "42",
+       //       },
+       // Whether this is a POST or GET request
+       type: "GET",
+       // The type of data we expect back
+       //dataType : "json",
+       // What do we do when the api call is successful
+       //   all the action goes in here
+       success: function(data) {
+           // do stuff
+           console.log(data);
+           $("#output").text(data);
+       },
+       // What we do if the api call fails
+       error: function (jqXHR, textStatus, errorThrown) {
+           // do stuff
+           console.log("Error:", textStatus, errorThrown);
+       }
+   })
+ })
